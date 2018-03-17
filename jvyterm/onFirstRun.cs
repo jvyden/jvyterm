799c242c7d7dc129c2c2cf34a550277cbe11874c
Jayden Rivett <jvyden420@gmail.com>
2018-03-17 19:14:14 -0400

    Comment out the auto move dll thing as its not working.

--- jvyterm/onFirstRun.cs
+++ jvyterm/onFirstRun.cs
@@ -8,13 +8,14 @@ namespace jvyterm
         {
             Directory.CreateDirectory(f);
             Directory.CreateDirectory(f + @"\plugins\");
-            System.Threading.Thread.Sleep(500);
+            /*System.Threading.Thread.Sleep(500);
             DirectoryInfo d = new DirectoryInfo(Environment.CurrentDirectory);
             FileInfo[] i = d.GetFiles("*.dll");
             foreach(FileInfo fi in i)
             {
                 fi.CopyTo(f + @"\plugins\");
             }
+            */
         }
     }
 }
