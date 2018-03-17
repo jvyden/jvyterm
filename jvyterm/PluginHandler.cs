c6528f4d21da7c360c1f2ec63a1371a733061d7b
Jayden Rivett <jvyden420@gmail.com>
2018-03-17 10:55:15 -0400

    Begin work on PluginHandler

---
+++ jvyterm/PluginHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace jvyterm
+{
+    class PluginHandler
+    {
+        public static void init()
+        {
+            logger.log("Initializing plugins...");
+            string pluginf = Environment.CurrentDirectory + "\\data\\plugins\\";
+            getAllDLLs(pluginf);
+        }
+
+        static FileInfo[] getAllDLLs(string dir)
+        {
+            DirectoryInfo d = new DirectoryInfo(dir);
+            FileInfo[] dlls = d.GetFiles("*.dll");
+            return dlls;
+        }
+    }
+}
