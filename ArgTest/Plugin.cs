e52252f1ff96e2dee6ba50a5fc530ac7782ddfe2
Jayden Rivett <jvyden420@gmail.com>
2018-03-18 13:08:58 -0400

    Added echo command to ArgTest.

--- ArgTest/Plugin.cs
+++ ArgTest/Plugin.cs
@@ -2,7 +2,16 @@
 
 namespace ArgTest
 {
-    public class Plugin
+    public class Plugin //For testing arguments for those who want to implement
     {
+        public static void init(string[] args = null)
+        {
+            CommandHandler.addCommand("echo");
+        }
+
+        public static void echo(string[] args = null)
+        {
+            logger.log(args.ToString(), logger.LogType.Regular);
+        }
     }
 }
